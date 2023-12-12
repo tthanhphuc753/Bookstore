cc6a024516cd1256d33e4e4c3cd6fa11ec3785bd
carwynserein <95391810+carwynserein@users.noreply.github.com>
2023-12-12 17:49:25 +0700

    update UserController

--- src/main/java/com/example/Bookstore/Presentation/Controller/userController/UserController.java
+++ src/main/java/com/example/Bookstore/Presentation/Controller/userController/UserController.java
@@ -1,11 +1,14 @@
 package com.example.Bookstore.Presentation.Controller.userController;
 
+import com.example.Bookstore.Domain.Model.Book.Book;
 import com.example.Bookstore.Domain.Model.User.User;
 import com.example.Bookstore.Domain.UserService.UserServices;
 import com.example.Bookstore.Presentation.Controller.BookController.BookController;
 import com.example.Bookstore.Presentation.Controller.CategoriesController.CategoriesServiceController;
 import com.example.Bookstore.Presentation.Controller.OrderController.OrderController;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -38,8 +41,9 @@ public class UserController {
 
 
     @GetMapping("/homepage")
-    public String showHomePage(Model model) {
-        model.addAttribute("books", bookController.getAllBook());
+    public String showHomePage(Model model,@RequestParam(defaultValue = "0") int page,
+                               @RequestParam(defaultValue = "12") int size) {
+        bookController.pageSize(model, page, size);
         model.addAttribute("category", categoriesServiceController.getAllCategory());
         return "homePage";
     }
